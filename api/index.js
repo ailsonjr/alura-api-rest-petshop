9eb51914ec33b5b03458e0e056e462f6502777b8
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-08-30 17:53:41 -0300

    Persistindo dados

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const router = require('./rotas/fornecedores');
 
 app.use(express.json());
 
-app.use('/api/fornecedores', router)
+app.use('/api/fornecedores', router);
 
 app.listen(config.get('api.port'), () => console.log('A API está funcionando'));
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor');
+const Fornecedor = require('./Fornecedor');
 
-router.use('/', async (req, res) => {
+
+router.get('/', async (req, res) => {
   const resultados = await TabelaFornecedor.listar();
   res.send(
     JSON.stringify(resultados)
+  );
+});
+
+router.post('/', async (req, res) => {
+  const reciveData = req.body;
+  const fornecedor = new Fornecedor(reciveData);
+  await fornecedor.criar();
+  res.send(
+    JSON.stringify(fornecedor)
   );
 });
 
